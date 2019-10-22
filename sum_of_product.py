0a041d29b0cd4beeaa7a0a4a37af7a01abcf44f9
17cs45 <53352139+17cs45@users.noreply.github.com>
2019-10-23 00:48:41 +0500

    2nd_Update

--- sum_of_product.py
+++ sum_of_product.py
@@ -6,8 +6,9 @@ def sum():
         if i %3 == 0 or i%5 == 0:
             total+=i
     return total
-<<<<<<< HEAD
 print(f'sum of multiples of 3 and 5 = {sum()}')
 =======
 print('sum of multiples of 3 and 5 =',sum())
->>>>>>> refs/remotes/origin/master
+
+
+# Bilawal Abbasi

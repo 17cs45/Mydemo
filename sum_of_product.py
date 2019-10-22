e9c5de7f4d43bbcb394ac9715dfdec62cff91fd3
= <=>
2019-10-23 00:45:19 +0500

    added sum_of_product.py

---
+++ sum_of_product.py
@@ -0,0 +1,11 @@
+def sum():
+    total = 0
+    for i in range(1,1000):
+        if i %3 == 0 or i%5 == 0:
+            total+=i
+    return total
+<<<<<<< HEAD
+print(f'sum of multiples of 3 and 5 = {sum()}')
+=======
+print('sum of multiples of 3 and 5 =',sum())
+>>>>>>> refs/remotes/origin/master
